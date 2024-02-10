0e6731812f828acae19dd5af0f83a55f1db97f32
Mohammed Ashraf <141479986+mohammedashraf16@users.noreply.github.com>
2024-02-11 00:43:15 +0200

    first commit

---
+++ lib/core/utlis/app_assets.dart
@@ -0,0 +1,36 @@
+class Assets {
+  Assets._();
+
+  /// Assets for imagesImagesForgetPassword
+  /// assets/images/imagesForgetPassword.png
+  static const String imagesImagesForgetPassword =
+      "assets/images/imagesForgetPassword.png";
+
+  /// Assets for imagesOnboarding1
+  /// assets/images/onboarding1.png
+  static const String imagesOnboarding1 = "assets/images/onboarding1.png";
+
+  /// Assets for imagesOnboarding2
+  /// assets/images/onboarding2.png
+  static const String imagesOnboarding2 = "assets/images/onboarding2.png";
+
+  /// Assets for imagesOnboarding3
+  /// assets/images/onboarding3.png
+  static const String imagesOnboarding3 = "assets/images/onboarding3.png";
+
+  /// Assets for imagesOrder Success
+  /// assets/images/order Success.png
+  static const String imagesOrderSuccess = "assets/images/order Success.png";
+
+  /// Assets for imagesVector 3
+  /// assets/images/Vector 3.svg
+  static const String imagesVector3 = "assets/images/Vector 3.svg";
+
+  /// Assets for imagesVector1
+  /// assets/images/vector1.svg
+  static const String imagesVector1 = "assets/images/vector1.svg";
+
+  /// Assets for imagesVector2
+  /// assets/images/Vector2.svg
+  static const String imagesVector2 = "assets/images/Vector2.svg";
+}
