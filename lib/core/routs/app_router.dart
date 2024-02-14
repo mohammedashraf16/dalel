0af7f7d4b84d4f4cb5cc192bb3b36942dada557d
Mohammed Ashraf <141479986+mohammedashraf16@users.noreply.github.com>
2024-02-14 22:36:20 +0200

    add firebase to our project

--- lib/core/routs/app_router.dart
+++ lib/core/routs/app_router.dart
@@ -1,6 +1,9 @@
+import 'package:dalel/core/services/service_locator.dart';
+import 'package:dalel/features/auth/presentation/auth_cubit/cubit/auth_cubit.dart';
 import 'package:dalel/features/auth/presentation/views/sign_up_view.dart';
 import 'package:dalel/features/ondoarding/presentation/view/onboarding_view.dart';
 import 'package:dalel/features/splash/presentation/splash_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter router = GoRouter(routes: [
@@ -14,10 +17,16 @@ final GoRouter router = GoRouter(routes: [
   ),
   GoRoute(
     path: "/SignUp",
-    builder: (context, state) => const SignUpView(),
+    builder: (context, state) => BlocProvider(
+      create: (context) => getIt<AuthCubit>(),
+      child: const SignUpView(),
+    ),
   ),
   GoRoute(
     path: "/SignIn",
-    builder: (context, state) => const SignUpView(),
+    builder: (context, state) => BlocProvider(
+      create: (context) => getIt<AuthCubit>(),
+      child: const SignUpView(),
+    ),
   ),
 ]);
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,16 @@ import 'package:dalel/core/database/cashe/cache_helper.dart';
 import 'package:dalel/core/routs/app_router.dart';
 import 'package:dalel/core/services/service_locator.dart';
 import 'package:dalel/core/utlis/app_color.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   setupServiceLocator();
   await getIt<CacheHelper>().init();
   runApp(const Dalel());
